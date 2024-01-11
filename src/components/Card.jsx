5c4ede542c13a9dc96f100c9fcfd3731c5373296
lennardnarci <148294228+lennardnarci@users.noreply.github.com>
2024-01-11 09:32:21 +0100

    refactor: Switched api call from card to weather results

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,6 @@
 import React from "react";
-import { useState, useEffect } from "react";
 
-const Card = ({ index, isOpen, searchValue, title }) => {
-  const [weatherData, setWeatherData] = useState(null);
-
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      try {
-        const apiKey = "0e17d6dc152f4c4da11184526240901";
-        const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchValue}&days=7`;
-
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-        console.log(data);
-        setWeatherData(data);
-      } catch (error) {
-        console.error("Error fetching weather data:", error);
-      }
-    };
-
-    fetchWeatherData();
-  }, [searchValue]);
-
+const Card = ({ index, isOpen, title, weatherData }) => {
   return (
     <div className={`card ${isOpen ? "open" : ""}`} key={index}>
       {weatherData && (
--- src/components/WeatherResults.jsx
+++ src/components/WeatherResults.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import { useState, useEffect } from "react";
 
 const now = new Date();
 const days = [
@@ -18,6 +19,27 @@ const WeatherResults = ({ searchValue }) => {
     return days[(now.getDay() + index) % totalDays];
   };
 
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const apiKey = "0e17d6dc152f4c4da11184526240901";
+        const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchValue}&days=7`;
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        console.log(data);
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    fetchWeatherData();
+  }, [searchValue]);
+
   return (
     <div className="weather-results">
       <Card
@@ -25,42 +47,49 @@ const WeatherResults = ({ searchValue }) => {
         isOpen={true}
         searchValue={searchValue}
         title={getDayByIndex(0)}
+        weatherData={weatherData}
       />
       <Card
         index="1"
         isOpen={false}
         searchValue={searchValue}
         title={getDayByIndex(1)}
+        weatherData={weatherData}
       />
       <Card
         index="2"
         isOpen={false}
         searchValue={searchValue}
         title={getDayByIndex(2)}
+        weatherData={weatherData}
       />
       <Card
         index="3"
         isOpen={false}
         searchValue={searchValue}
         title={getDayByIndex(3)}
+        weatherData={weatherData}
       />
       <Card
         index="4"
         isOpen={false}
         searchValue={searchValue}
         title={getDayByIndex(4)}
+        weatherData={weatherData}
       />
       <Card
         index="5"
         isOpen={false}
         searchValue={searchValue}
         title={getDayByIndex(5)}
+        weatherData={weatherData}
       />
       <Card
         index="6"
         isOpen={false}
         searchValue={searchValue}
         title={getDayByIndex(6)}
+        weatherData={weatherData}
       />
     </div>
   );
