b335a69c32cb1eb7956f258a80a4b89bb8016d7d
lennardnarci <148294228+lennardnarci@users.noreply.github.com>
2024-01-13 01:30:03 +0100

    fixed icons

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -30,7 +30,7 @@ const SearchBar = ({ onSearchValueChange }) => {
         onClick={() => setSearchValue("")}
       />
       <button className="search-button" onClick={handleClick}>
-        <img src="../../public/icons/arrow.svg" alt="Search Button" />
+        <img src="./arrow.svg" alt="Search Button" />
       </button>
     </div>
   );
